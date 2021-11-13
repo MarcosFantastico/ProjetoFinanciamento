b918d23bc3e09ceab0423f3e850b170030362bc7
MarcosFantastico <lima.vcl.com.br@gmail.com>
2021-11-12 22:13:42 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = window.document.querySelector('input#calcular');
+const resultado = window.document.getElementById('result');
+
+
+function financiamento(){
+    resultado.innerHTML = '<p>pppppp</p>';
+
+}
+button.addEventListener('click', financiamento);
