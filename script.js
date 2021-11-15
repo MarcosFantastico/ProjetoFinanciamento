1a07b589df8c341cf5b0cbd749ac5b606985c52c
MarcosFantastico <lima.vcl.com.br@gmail.com>
2021-11-15 15:45:33 -0300

    last commit

--- script.js
+++ script.js
@@ -3,7 +3,30 @@ const resultado = window.document.getElementById('result');
 
 
 function financiamento(){
-    resultado.innerHTML = '<p>pppppp</p>';
+    const nome = window.document.getElementById('nome').value;
+    const renda_mensal = parseFloat(window.document.querySelector('input#renda_mensal').value);
+    var valor_do_bem = parseFloat(window.document.querySelector('input#valor_do_bem').value);
+    const valor_entrada = parseFloat(window.document.querySelector('input#valor_entrada').value);
+    const taxa_juros = parseFloat(window.document.querySelector('input#taxa_jurus').value);
+    const prestacoes = parseInt(window.document.querySelector('input#prestacoes').value);
+
+    
+    valor_do_bem = valor_do_bem - valor_entrada;
+
+    const financiamento = (valor_do_bem*(1 + (taxa_juros/100))**prestacoes*(taxa_juros/100)/((1+ (taxa_juros/100))**prestacoes - 1)).toFixed(2)
+
+    const renda_30_porcento = parseFloat(renda_mensal) * 0.3;
+
+
+    if (renda_30_porcento > financiamento)
+
+    {
+    resultado.innerHTML = `<p>Olá ${nome}, Parabéns seu financiamento foi aprovado! o valor de 30% de sua renda mensal é: R$${renda_30_porcento}. As prestações do seu fianciamento são de: R$${financiamento}</p>`;
+    }
+    else
+    {
+        resultado.innerHTML = `<p>Olá ${nome}! Infelizmente você não consiguirá realizar o financiamento porque 30% de sua renda mensal é: R$${renda_30_porcento} e as prestações são de: R$${financiamento}`
+    }
 
 }
 button.addEventListener('click', financiamento);
